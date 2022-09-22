c3c0a64c07bb8e71c32d62e2f75f87b1880f60c5
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-22 19:48:31 -0300

    Challenge Finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Movie[],
+  selectedGenre: {
+    title: string;
+  }
+}
+
+export function Content({movies, selectedGenre}: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface Genre {
+  id: number;
+      name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+      title: string;
+}
+
+interface SideBarProps { 
+  genres: Genre[],
+  onClickButton: (genreId: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({genres, onClickButton, selectedGenreId}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  );
 }
